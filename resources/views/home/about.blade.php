7f620eb8609a6e84de386deada69a84ce8128cac
yazen <nooryazen@gmail.com>
2020-07-28 22:34:54 +0300

    project files

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -2,7 +2,14 @@
     <div class="jumbotron">
         <h1>ZainCash Test - Solution Presentation</h1>
         <p class="lead">
-            The website is an implementation of the technical solution to the tasks/problems of ZainCash employment Test.
+            The website is an implementation of the technical solution to the tasks/problems of ZainCash employment Test. The coding task solution is programmed using Laravel while the database task solution follows MySQL syntax.
+        </p>
+
+        <p class="lead font-weight-bold">
+            Candidate:
+        </p>
+        <p class="lead">
+            Yazen AL-Khalaf <br>  Full Stack Web and Mobile Developer <br> +964 773 203 1244 <br> Erbil, Iraq
         </p>
     </div>
 </main>
